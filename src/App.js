cf2aee60978b0a99c77d5dba934c317a3a770c5a
mun5424 <mun5424@gmail.com>
2023-06-13 12:51:21 -0700

    be able to shuffle cards with handled states, shuffle cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Button from'react-bootstrap/Button';
 import food from './data/food.json';
@@ -15,20 +14,20 @@ import Dropdown from 'react-bootstrap/Dropdown';
 
 export default function MyApp() {
 
-  function shuffleCards(foods) {
+  const jsonFoods = food.food;
+
+  const [foods, setFoods] = useState(jsonFoods);
+
+  function shuffleCards() {
     for (let i = 0; i < foods.length - 1; i++) {
       let j = i + Math.floor(Math.random() * (foods.length - i));
       let temp = foods[j];
       foods[j] = foods[i];
       foods[i] = temp;
     }
-
-    return foods;
+    setFoods([...foods]);
   };
 
-  const jsonFoods = food.food;
-
-  const [foods, setFoods] = useState(shuffleCards(jsonFoods));
 
   function sortBy(orderType) {
     const sorted = [...foods].sort((a, b) => {
@@ -39,68 +38,71 @@ export default function MyApp() {
 
 
   function renderFoodCards(foods) {
-    return foods.map(item=> (
-      <Card className="card" style={{ width: '18rem' }}>
-            <div className="image-container">
-              <img className="item-image" src={item.imageURL}></img>
-            </div>
-            <div className="card-body">
-              <div className="card-title-container">
-                <div className="card-title-logo">
-                  <img className="logo" src={item.logoURL}></img>
+    return (
+      <div className="cards-container">
+        { foods.map(item=> (
+          <Card className="card" style={{ width: '18rem' }}>
+                <div className="image-container">
+                  <img className="item-image" src={item.imageURL}></img>
                 </div>
-                <div className="card-title-text">
-                  <span className="item-title"> {item.name}</span>
-                </div>
-              </div>
                 <div className="card-body">
-                  <div className="calories detail-justify font"> 
-                  <div>Calories</div>
-                  <div>{item.calories}kcal</div> 
+                  <div className="card-title-container">
+                    <div className="card-title-logo">
+                      <img className="logo" src={item.logoURL}></img>
+                    </div>
+                    <div className="card-title-text">
+                      <span className="item-title"> {item.name}</span>
+                    </div>
                   </div>
-                  <div className="totalfat detail-justify font">
-                    <div>Total Fat</div>
-                    <div>{item.totalFat}g</div> 
-                  </div>
-                  <div className="saturatedfat detail-justify font">
-                    <div>Sat Fat</div>
-                    <div>{item.saturatedFat}g</div> 
-                  </div>
-                  <div className="cholesterol detail-justify font">
-                    <div>Cholesterol</div>
-                    <div>{item.cholesterol}mg</div>
+                    <div className="card-body">
+                      <div className="calories detail-justify font"> 
+                      <div>Calories</div>
+                      <div>{item.calories}</div> 
+                      </div>
+                      <div className="totalfat detail-justify font">
+                        <div>Total Fat</div>
+                        <div>{item.totalFat}g</div> 
+                      </div>
+                      <div className="saturatedfat detail-justify font">
+                        <div>Sat Fat</div>
+                        <div>{item.saturatedFat}g</div> 
+                      </div>
+                      <div className="cholesterol detail-justify font">
+                        <div>Cholesterol</div>
+                        <div>{item.cholesterol}mg</div>
+                        </div>
                     </div>
                 </div>
-            </div>
-      </Card>
-    ));
+          </Card>
+      ))
+    }
+    </div>
+    )
   }
 
-  function sortByComponent() {
+  function buttonComponents() {
     return (
-      <div className="dropdown-container"> 
-        <DropdownButton id="dropdown-basic-button" title="Sort By">
-          <Dropdown.Item onClick={() => sortBy("calories")}>Calories </Dropdown.Item>
-          <Dropdown.Item onClick={() => sortBy("totalFat")}>Total Fat</Dropdown.Item>
-          <Dropdown.Item onClick={() => sortBy("saturatedFat")}>Saturated Fat</Dropdown.Item>
-          <Dropdown.Item onClick={() => sortBy("cholesterol")}>Cholesterol</Dropdown.Item>
-        </DropdownButton>
-      </div> 
-    );
-  }
-
-  function recommendButton() {
-    return (
-      <div className="button-container"> 
-        <Button>
-          Shuffle
-        </Button>
+      <div className="buttons-container">
+        <div className="button-container"> 
+          <Button onClick={() => shuffleCards()}>
+            Shuffle
+          </Button>
+        </div>
+        <div className="dropdown-container"> 
+          <DropdownButton id="dropdown-basic-button" title="Sort By">
+            <Dropdown.Item onClick={() => sortBy("calories")}>Calories </Dropdown.Item>
+            <Dropdown.Item onClick={() => sortBy("totalFat")}>Total Fat</Dropdown.Item>
+            <Dropdown.Item onClick={() => sortBy("saturatedFat")}>Saturated Fat</Dropdown.Item>
+            <Dropdown.Item onClick={() => sortBy("cholesterol")}>Cholesterol</Dropdown.Item>
+          </DropdownButton>
+        </div> 
+          
       </div>
     );
   }
 
   return (
-    <Container>
+    <Container className="container">
       <Navbar className="header-font-bold" expand="lg">
           <Navbar.Brand href="#home"> Nutritious.ly </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav"/>
@@ -113,8 +115,7 @@ export default function MyApp() {
           </Navbar.Collapse>
       </Navbar>
       <Row>
-        {sortByComponent()}
-        {recommendButton()}
+        {buttonComponents()}
       </Row>
       <Row>
         {renderFoodCards(foods)}
